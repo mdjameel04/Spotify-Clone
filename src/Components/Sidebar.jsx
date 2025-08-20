0ea228100c65eb09f2efacc74cbd2ca4e4622462
mdjameel04 <jameeltony67@gmail.com>
2025-08-20 17:45:12 +0530

    first commit

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -13,7 +13,13 @@ const Sidebar = () => {
     <p className='font-bold'>search</p>
       </div>
         </div> 
-        
+        <div className="bg-[#121212] h-[85%] rounded ">
+            <div className="p-4 flex items-center justify-between ">
+                <div className="flex items-center gap-3">
+                    <img src={assets.stack_icon} alt="" />
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
