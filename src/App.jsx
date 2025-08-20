20361c570fbb5fee51507b785a81595f416214a8
mdjameel04 <jameeltony67@gmail.com>
2025-08-20 17:38:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from './Components/Sidebar'
+
+const App = () => {
+  return (
+    <div className='h-screen bg-black'>
+      <div className='h-[90%] flex'>
+         <Sidebar/>
+      </div>
+    </div>
+  )
+}
+
+export default App
