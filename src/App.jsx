fe64874c132cd4552419ba9b1fbc8d6ddfb86e62
mdjameel04 <jameeltony67@gmail.com>
2025-08-21 10:43:37 +0530

    added files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Sidebar from './Components/Sidebar'
+import Player from './Components/Player'
 
 const App = () => {
   return (
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
-         <Sidebar/>
+         <Sidebar/>    
       </div>
+      <Player/>
     </div>
   )
 }
