1108b096ac6d2960e0ce687ca956b4e8a36d07f7
mdjameel04 <jameeltony67@gmail.com>
2025-08-22 11:15:44 +0530

    inital commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from './Navbar'
-
+import {albumsData} from '../assets/assets.js'
+import AlbumItem from './AlbumItem'
 const Home = () => {
   return (
     <>
       <Navbar/>
       <div className='my-5 font-bold text-2xl'>
-        
+     {albumsData.map((item,index)=>(
+       <AlbumItem  key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>
+     ))}
       </div>
     </>
   )
