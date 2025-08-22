6716b064d5cc1ecf5ba7b2f5302bc9c192b18b45
mdjameel04 <jameeltony67@gmail.com>
2025-08-22 16:07:32 +0530

    inital commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,15 +2,29 @@ import React from 'react'
 import Navbar from './Navbar'
 import {albumsData} from '../assets/assets.js'
 import AlbumItem from './AlbumItem'
+import { songsData } from '../assets/assets.js'
+import SongItem from './SongItem.jsx'
 const Home = () => {
   return (
     <>
       <Navbar/>
-      <div className='my-5 font-bold text-2xl'>
-     {albumsData.map((item,index)=>(
+      <div className='mb-4'>
+        <h1 className='my-5 font-bold text-2xl'> Featured Charts</h1>
+        <div className='flex overflow-auto'>
+{albumsData.map((item,index)=>(
        <AlbumItem  key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>
      ))}
       </div>
+        </div>
+     
+          <div className='mb-4'>
+        <h1 className='my-5 font-bold text-2xl'> Today's biggest hit</h1>
+        <div className='flex overflow-auto gap-4'>
+          {songsData.map((item,index)=>( 
+            <SongItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />
+          ))}
+      </div>
+        </div>
     </>
   )
 }
---
+++ src/Components/SongItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SongItem = ({name, image, desc,id}) => {
+  return (
+    <div className='min-w-[180px] rounded cursor-pointer hover:bg-[#ffffff26] ]'>
+      <img src={image} alt="" />
+      <p  className='font-bold mt-2 mb-1'>{name}</p>
+      <p  className='text-slate-200 text-sm'>{desc}</p>
+    </div>
+  )
+}
+
+export default SongItem
