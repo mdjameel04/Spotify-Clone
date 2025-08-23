06d34dee0be7c3e70cdb370db5f4a37f0c3a1f59
mdjameel04 <jameeltony67@gmail.com>
2025-08-23 16:46:51 +0530

    added files

---
+++ src/Context/PlayerContext.jsx
@@ -0,0 +1,54 @@
+import { createContext, useRef, useState } from "react";
+import { songsData } from "../assets/assets";
+
+export const PlayerContext = createContext();
+
+const PlayerContextProvider = (props) => {
+  const audioRef = useRef();
+  const seekBg = useRef();
+  const seekBar = useRef();
+
+  const [track, setTrack] = useState(songsData[0]);
+  const [playStatus, setplayStatus] = useState(false);
+  const [time, setTime] = useState({
+    currentTime: {second: 0, minute: 0 },
+    totalTime: { second: 0, minute: 0 },
+  });
+
+  const play = () => {
+   
+      audioRef.current.play();
+      setplayStatus(true);
+    
+  };
+
+  const pause = () => {
+   
+      audioRef.current.pause();
+      setplayStatus(false);
+    
+  };
+
+  const contextValue = {
+    audioRef,
+    seekBar,
+    seekBg,
+    track,
+    setTrack,
+    playStatus,
+    setplayStatus,
+    time,
+    setTime,
+    play,
+    pause,
+  };
+
+  return (
+    <PlayerContext.Provider value={contextValue}>
+      {props.children}
+    </PlayerContext.Provider>
+  );
+};
+
+export default PlayerContextProvider;
+
